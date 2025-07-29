8477c1831fea754a883a4ba096ae2a67503b03aa
offrahul <offrahul995@gmail.com>
2025-07-30 00:20:51 +0530

    add register and controler

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,13 +1,80 @@
 
 import {asyncHandler} from "../utils/ashnchandler.js";
-
+import { ApiError } from "../utils/ApiError.js";
+import {User} from "../models/user.model.js"
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 const registerUser=asyncHandler(async(req,res,next)=>{
     // const user=await User.create(req.body)
-    res.status(200).json({ 
-        message:"User created successfully in website",
+    // res.status(200).json({ 
+    //     message:"User created successfully in website",
        
-    })
+    // })
+
+
+    //step to register user
+    //1. get user from frontend or postman
+    //2. validation - not empty
+    //3. check if user already exist : username,email
+    //4. check for image and for avatar
+    //upload to cloudinary,avatar
+    //create user object-create entry in db
+    //remove password and refesh token from response
+    //check user creation
+    //return for response
+
+    const {fullName,username,email,password}=req.body
+   if(
+    [fullName,username,email,password].some((fields)=>fields?.trim()==="")
+   ){
+    throw new ApiError("Please fill all the fields",400)
+   }
+    
+
+   //step 2
+  const existedUser= User.findOne({
+    $or:[{username},{email}]
+   
+   })
+
+   if(existedUser){
+    throw new ApiError("User already exist",409)
+   }
+   //step 3
+
+   const avatarLoacalPath= req.files?.avatar[0]?.path;
+ const coverageLoacalPath=  req.files?.coverImage[0]?.path
+
+if(!avatarLoaclPath){
+    throw new ApiError("Please upload avatar",400)
+}
+//upload in cloudinary
+const avatar= await uploadOnCloudinary(avatarLoacalPath);
+const coverImage= await uploadOnCloudinary(coverageLoacalPath);
+
+if(!avatar){
+    throw new ApiError("Please upload avatar and cover image",400)
+}
+//create object and entry in db 
+const user= await User.create({
+    fullName,
+    username:username.toLowerCase(),
+    email,
+    password,
+    avatar:avatar.url,
+     coverImage:coverImage?.url || ""
+})
+//remove password from user object
+const createdUser=await User.findByid(user._id).select("-password -__refreshToken")
+
+if(!createdUser){
+    throw new ApiError("something ent user while registering the user",500)
+}
+
+//response
+return res.status(201).json(new ApiResponse("User created successfully",createdUser,200))
+
 })
 
 export {registerUser}
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -29,7 +29,7 @@ const userSchema=new Schema({
         type:String,//cloudinary url
         required:true,
     },
-    coverageImage:{
+    coverImage:{
          type:String,//cloudinary url
     },
     watchHistory:[
